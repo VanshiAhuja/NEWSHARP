d5583becffbf76fe51e03f70920f61180077002c
Sanjay Ailani <sailani74@gmail.com>
2023-07-14 14:40:52 +0530

    Add project files.

---
+++ NEWSHARP/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace NEWSHARP
+{
+    partial class COM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // COM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NEWSHARP.Properties.Resources.PPPPPPPPPP;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Name = "COM";
+            this.Text = "COMPANY SELECTION";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
